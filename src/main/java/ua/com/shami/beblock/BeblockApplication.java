d8332aba3465e4b5a9d7c471e6771287a9e36045
Mykhailo Marchenko <mykhailo.marchenko@nearshorepartner.ch>
2020-02-29 10:47:23 +0200

    Init app

---
+++ src/main/java/ua/com/shami/beblock/BeblockApplication.java
@@ -0,0 +1,13 @@
+package ua.com.shami.beblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeblockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/shami/beblock/BeblockApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.shami.beblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeblockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
